d40847195b53de4a776f7e1c9bf9d8889c7fc7c4
wimbaertvlotbe <36698691+wimbaertvlotbe@users.noreply.github.com>
2021-09-20 08:56:46 +0200

    Add project files.

---
+++ Thema3_Klassen_en_Objecten_Opdracht2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Thema3_Klassen_en_Objecten_Opdracht2
+{
+    class Program
+    {
+    
+        /*
+            Thema3_Klassen_en_Objecten_Opdracht1 
+
+                In opdracht TSO - C#.NET THEMA3 : OOP - OPDRACHT1 heb je de code voor 
+                de klasse balk geschreven. 
+
+                Je hebt vastgesteld dat je van een gedeclareerd maar nog niet geïnstantieerd 
+                object de attributen niet kan bekijken omdat het object nog niet bestaat.
+                In deze opdracht ga je code voorzien om na te gaan of een object bestaat 
+                of niet. Je hebt tijdens de theorieles uitleg gekregen over REFERENCE TYPES.
+                Je zal die kennis hier moeten toepassen. 
+
+            OPGELET : 
+
+                Opdracht 1 moet afgewerkt zijn voordat je deze opdracht kan uitvoeren. 
+                Commit en push na elke deelopdracht.
+
+            OPDRACHT:
+
+            1. Maak een array aan dat tien objecten van de klasse Balk kan bevatten.
+            2. Zitten er na declaratie van het array al objecten in het array? 
+               Hoe kan je dit uittesten? Voorzie een codelijn
+            3. Voeg in een for-lus op de even locaties in het array een object toe. 
+               Wat wordt er precies in het array opgeslagen?
+            4. Hoe komt het dat je een object dat in de lus van vraag 4 is aangemaakt 
+               toch kan gebruiken buiten de sluit-accolade van de for-loop? Leg uit. 
+            5. Programmeer een lus die het array met de objecten afloopt. 
+               Voor elke arraylocatie geef je in het consolevenster een bericht of er 
+               een object zit of niet. Gebruik hiervoor -strings ($"...")
+            6. Hoe kan je een object uit het array verwijderen? Voorzie hiervoor code.
+
+    */
+
+
+        static void Main()
+        {
+            
+        }
+    }
+}
